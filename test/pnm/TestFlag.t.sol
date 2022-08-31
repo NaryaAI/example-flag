540372fd3b237ffc4462023eb06a632958ad1e92
tarafans <tarafans7@gmail.com>
2022-08-31 12:33:09 -0400

    Update

--- test/pnm/TestFlag.t.sol
+++ test/pnm/TestFlag.t.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
-import "./Agent.sol";
 import "contracts/Flag.sol";
+import "@pwnednomore/contracts/Agent.sol";
 
-contract FuzzFlag is Agent {
+contract TestFlag is Agent {
     Flag flag;
 
-    function setUp() public {
+    function setUp() external {
         flag = new Flag();
     }
 
-    function check() public view {
+    function check() external view override {
         require(flag.flag1(), "flag1 is false");
     }
 }
