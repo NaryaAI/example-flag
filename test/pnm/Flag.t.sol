358805347674a0de2ae436b389e137418aa1fa15
tarafans <tarafans7@gmail.com>
2022-09-19 23:29:02 -0400

    Fix syntax

--- test/pnm/Flag.t.sol
+++ test/pnm/Flag.t.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.0;
 import "contracts/Flag.sol";
 import "@pwnednomore/contracts/Agent.sol";
 
-contract TestFlag is Agent {
+contract FlagTest is Agent {
     Flag flag;
 
     function setUp() external {
         flag = new Flag();
     }
 
-    function check() external view override {
+    function invariantFlagIsTrue() external view {
         require(flag.flag1(), "flag1 is false");
     }
 }
